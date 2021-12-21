2bda7d926dd3179d94cc7b1eea3705a3c332ddb6
sgjh <sgjh@iMac.local>
2021-12-21 14:39:54 -0800

    create react app using unsplash api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
+import store from './store';
+
+import Home from './containers/Home';
+import UserPhotos from './containers/UserPhotos';
+
+import Footer from './containers/Footer';
+import SideBar from './containers/SideBar';
+
 import './App.css';
+
+const Wrapper = styled.div`
+  height: 100vh;
+`;
+const ContentWrapper = styled.div`
+  width: calc(100vw - 360px);
+  height: calc(100vh - 65px);
+  margin-left: 360px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store = { store }>
+      <Router>
+        <Wrapper>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>XOTV FrontEnd TEST</title>
+          </Helmet>
+          <Wrapper>
+            <SideBar />
+            <ContentWrapper>
+              <Routes>
+                <Route exact path="/photos" element={<UserPhotos />} />
+                <Route exact path="/" element={<Home />} />
+              </Routes>
+            </ContentWrapper>
+            <Footer />
+          </Wrapper>
+        </Wrapper>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/constants/service-info.js
@@ -0,0 +1,3 @@
+export const API_ROOT = 'https://api.unsplash.com';
+export const CLIENT_ID = 'pvQoYrZ94SCxLXiN5-f6djf4msBJLbf997umraDp1-w';
+export const CLIENT_SECRET = 'c6g6zF9zj6nuN4dIDVwyDKw5woj4u6nRkqdtKiDKBbk';
